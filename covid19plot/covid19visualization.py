8fed5dae9c0ad6e8777cee1c1377311cf35dddf8
baby1900 <60949144+baby1900@users.noreply.github.com>
2020-04-26 01:23:41 -0400

    Update covid19visualization.py
    change format

--- covid19plot/covid19visualization.py
+++ covid19plot/covid19visualization.py
@@ -6,16 +6,22 @@ import plotly.express as px
 import plotly.graph_objs as go
 from plotly.subplots import make_subplots
 from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+
+
 init_notebook_mode(connected=True)
+
 # Read Data
 df = pd.read_csv("data/covid_19_data.csv")
+
 # Rename columns
 df = df.rename(columns={'Country/Region':'Country'})
 df = df.rename(columns={'ObservationDate':'Date'})
+
 # Manipulate Dataframe
 df_countries = df.groupby(['Country', 'Date']).sum().reset_index().sort_values('Date', ascending=False)
 df_countries = df_countries.drop_duplicates(subset = ['Country'])
 df_countries = df_countries[df_countries['Confirmed']>0]
+
 # Create the Choropleth
 fig = go.Figure(data=go.Choropleth(
     locations = df_countries['Country'],
